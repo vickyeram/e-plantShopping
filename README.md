b5315fbe6257a55995ddf513387790adc79cd386
vickeyram <vickeyram61@gmail.com>
2026-01-27 14:20:49 +0530

    committed changes for add to cart, increment, decrement, total cost in cart

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# coding-project-template
+# e-plantShopping
+
+e-plantShopping is a React-based plant shopping application that allows users to browse plants, add items to a cart, manage item quantities, and view total cart items using Redux Toolkit.
+
+## Features
+- Browse plant products
+- Add plants to cart
+- Increase and decrease quantity
+- View total cart quantity
+
+## Technologies Used
+- React
+- Redux Toolkit
+- JavaScript
+- HTML
+- CSS
+
+## How to Run
+1. Clone the repository
+2. Run `npm install`
+3. Run `npm run dev`
--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -8,28 +8,37 @@ const CartItem = ({ onContinueShopping }) => {
   const dispatch = useDispatch();
 
   // Calculate total amount for all products in the cart
-  const calculateTotalAmount = () => {
- 
-  };
+  const calculateTotalAmount = () =>
+    cart.reduce((total, item) => {
+      const cost = Number(item.cost.slice(1));
+      return total + cost * item.quantity;
+    }, 0);
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e)
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name))
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return parseFloat(item.cost.substring(1)) * item.quantity
+  };
+
+  const handleCheckoutShopping = () => {
+    alert('Functionality to be added for future reference');
   };
 
   return (
@@ -57,7 +66,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={() => handleCheckoutShopping()}>Checkout</button>
       </div>
     </div>
   );
